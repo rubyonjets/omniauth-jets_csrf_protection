fc2b19870cd82d3ad12a0bee2f7651cc8f276782
Tung Nguyen <tongueroo@gmail.com>
2023-11-27 11:27:53 +0000

    first commit

---
+++ lib/omniauth/jets_csrf_protection.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "jets_csrf_protection/version"
+
+module Omniauth
+  module JetsCsrfProtection
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
